52ad7973ebb13f8dcb9a81f9df93110b2569de69
unknown <mursalinkhan552@gmail.com>
2024-01-31 11:26:47 +0600

    Added Context API Project

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Main from './components/Layout/Main.jsx';
+import OrderReview from './components/OrderReview/OrderReview.jsx';
+import Grandpa from './components/Grandpa/Grandpa.jsx';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: '/review',
+        element: <OrderReview></OrderReview>
+      },
+      {
+        path:'/grandpa',
+        element: <Grandpa></Grandpa>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
